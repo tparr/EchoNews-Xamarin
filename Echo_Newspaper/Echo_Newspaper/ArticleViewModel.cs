440d04a169a7c2f2f6a40ada6e29ad57927ee773
dum000 <Timmy_parr@yahoo.com>
2018-01-31 13:17:42 -0500

    first commit

---
+++ Echo_Newspaper/Echo_Newspaper/ArticleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Echo_Newspaper
+{
+    public class ArticleViewModel
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Date { get; set; }
+    }
+}
---
+++ Echo_Newspaper/Echo_Newspaper/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Echo_Newspaper
+{
+	public partial class MainPage : ContentPage
+	{
+	    public ObservableCollection<ArticleViewModel> Articles { get; set; }
+
+		public MainPage()
+		{
+		    InitializeComponent();
+
+		    Articles = new ObservableCollection<ArticleViewModel>
+		    {
+		        new ArticleViewModel {Title = "Title1", Date = "Today", Image = @"C:\Users\tparr\Documents\Visual Studio 2017\Projects\Echo_Newspaper\Echo_Newspaper\Echo_Newspaper\Headshot.jpg"},
+		        new ArticleViewModel {Title = "Title2", Date = "Yesterday", Image = "No Image"},
+		        new ArticleViewModel {Title = "Title3", Date = "2 Days Ago", Image = "No Image"}
+		    };
+
+		    listView.ItemsSource = Articles;
+		}
+	}
+}
